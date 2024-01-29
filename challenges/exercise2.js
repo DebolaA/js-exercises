2e073f20dff8ba8c6104861470a2c63715f6651f
Debola Adedapo <niikee80@hotmail.com>
2024-01-29 00:34:49 +0000

    feat:exercise 2 completed

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -1,26 +1,62 @@
 export function getSquares(nums) {
-	if (nums === undefined) throw new Error('nums is required');
-	// Your code here!
+    if (nums === undefined) throw new Error('nums is required')
+    // Your code here!
+    return nums.map((number) => number ** 2)
 }
 
 export function camelCaseWords(words) {
-	if (words === undefined) throw new Error('words is required');
-	// Your code here!
+    if (words === undefined) throw new Error('words is required')
+    // Your code here!
+    const finalCamelCaseWord = words.reduce(
+        (previousValue, currentValue) =>
+            `${previousValue}${
+                currentValue[0].toUpperCase() + currentValue.slice(1)
+            }`
+    )
+    return finalCamelCaseWord
 }
 
 export function getTotalSubjects(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+    if (people === undefined) throw new Error('people is required')
+    // Your code here!
+    const totalSubjects = 0
+    const sumWithTotal = people.reduce(
+        (acc, currentValue) => acc + currentValue.subjects.length,
+        totalSubjects
+    )
+    return sumWithTotal
 }
 
 export function checkIngredients(menu, ingredient) {
-	if (menu === undefined) throw new Error('menu is required');
-	if (!ingredient) throw new Error('ingredient is required');
-	// Your code here!
+    if (menu === undefined) throw new Error('menu is required')
+    if (!ingredient) throw new Error('ingredient is required')
+    // Your code here!
+    let hasIngredient = false
+    menu.forEach((menuItem) => {
+        let itemIndex = menuItem.ingredients.findIndex(
+            (item) => item === ingredient
+        )
+        if (itemIndex > -1) hasIngredient = true
+    })
+    return hasIngredient
 }
 
 export function duplicateNumbers(arr1, arr2) {
-	if (arr1 === undefined) throw new Error('arr1 is required');
-	if (arr2 === undefined) throw new Error('arr2 is required');
-	// Your code here!
+    if (arr1 === undefined) throw new Error('arr1 is required')
+    if (arr2 === undefined) throw new Error('arr2 is required')
+    // Your code here!
+    const arrOfDuplicateNumbers = arr1.filter((num) => {
+        const index = arr2.findIndex((x) => x === num)
+        if (index > -1) return arr2[index]
+    })
+    const arrOfUniqueNumbers = removeDuplicateNumbers(arrOfDuplicateNumbers)
+    return arrOfUniqueNumbers.sort(compareNumbers)
+}
+
+function compareNumbers(a, b) {
+    return a - b
+}
+
+function removeDuplicateNumbers(arr) {
+    return [...new Set(arr)]
 }
